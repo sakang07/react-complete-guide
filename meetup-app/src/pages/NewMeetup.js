965a366816eff07dc5ee90b1cae7288f397d042f
SA K <sakang07@gmail.com>
2022-02-01 02:46:18 +0900

    [step_08] Set http request and feedback logic

--- meetup-app/src/pages/NewMeetup.js
+++ meetup-app/src/pages/NewMeetup.js
@@ -1,10 +1,24 @@
+import { useHistory } from 'react-router-dom';
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 function NewMeetup() {
+  const history = useHistory();
+
+  function onAddMeetupHandler(meetupData) {
+    fetch('https://react-getting-started-7db92-default-rtdb.firebaseio.com/meetups.json', {
+      method: 'POST',
+      body: JSON.stringify(meetupData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then(() => {
+      history.replace('/');
+    });
+  }
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={onAddMeetupHandler} />
     </section>
   );
 }
