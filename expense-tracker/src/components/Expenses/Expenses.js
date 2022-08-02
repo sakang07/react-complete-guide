ed3521159caf4bc5241e8b61f29a3925e1acdc54
SA K <sakang07@gmail.com>
2022-08-03 00:58:28 +0900

    [section 5-69] Map items and use conditional rendering
    TODO: Review 67-69(Chart part)

--- expense-tracker/src/components/Expenses/Expenses.js
+++ expense-tracker/src/components/Expenses/Expenses.js
@@ -1,23 +1,45 @@
-import { useState } from "react";
-import ExpenseFilter from "./ExpenseFilter";
-import ExpenseItem from "./ExpenseItem";
-import Card from "../UI/Card";
-import "./Expenses.css";
+import { useState } from 'react';
+import ExpenseFilter from './ExpenseFilter';
+import ExpenseItem from './ExpenseItem';
+import Card from '../UI/Card';
+import './Expenses.css';
+import ExpenseseList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
 
 const Expenses = props => {
-  const [filteredYear, setFilteredYear] = useState("2020");
+  const [filteredYear, setFilteredYear] = useState('2020');
 
   const filterChangeHandler = selectedYear => {
     setFilteredYear(selectedYear);
   };
 
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
     <div>
-      <Card className='expenses'>
+      <Card className="expenses">
         <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        {props.items.map(expense => (
-          <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} />
-        ))}
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpenseseList items={filteredExpenses} />
+
+        {/* 2. && 사용하여 조건부 렌더링 */}
+        {/* 앞의 조건을 만족할 경우 && 뒤의 조건이 출력된다 */}
+        {/* {filteredExpenses.length === 0 && <p>No expenses found.</p>}
+        {filteredExpenses.length > 0 &&
+          filteredExpenses.map(expense => (
+            <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
+          ))} */}
+
+        {/* 2. 삼항연산자 사용하여 조건부 렌더링 */}
+        {/* {filteredExpenses.length === 0 ? (
+          <p>No expenses found.</p>
+        ) : (
+          filteredExpenses.map(expense => (
+            <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
+          ))
+        )} */}
       </Card>
     </div>
   );
