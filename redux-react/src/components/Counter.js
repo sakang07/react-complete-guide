08c911dc7a9d683222df31085b2746897b1de7e8
SA K <sakang07@gmail.com>
2022-09-25 22:32:42 +0900

    [section 18-251] redux state control

--- redux-react/src/components/Counter.js
+++ redux-react/src/components/Counter.js
@@ -6,22 +6,31 @@ const Counter = () => {
   const dispatch = useDispatch();
   // useSelector - 자동으로 구독을 설정해서 데이터가 바뀔 때마다 받아온다
   const counter = useSelector(state => state.counter);
+  const show = useSelector(state => state.showCounter);
 
   const incrementHandler = () => {
     dispatch({ type: 'increment' });
   };
+
+  const increaseHandler = () => {
+    dispatch({ type: 'increase', amount: 5 });
+  };
+
   const decrementHandler = () => {
     dispatch({ type: 'decrement' });
   };
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: 'toggle' });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increment by 5</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
--- redux-react/src/store/index.js
+++ redux-react/src/store/index.js
@@ -1,23 +1,41 @@
 import { createStore } from 'redux';
 
+const initialState = { counter: 0, showCounter: true };
+
 // reducer 생성
-const counterReducer = (state = { counter: 0 }, action) => {
+const counterReducer = (state = initialState, action) => {
   if (action.type === 'increment') {
-    return { counter: state.counter + 1 };
+    return {
+      counter: state.counter + 1,
+      showCounter: state.showCounter,
+    };
   }
+
+  if (action.type === 'increase') {
+    return {
+      counter: state.counter + action.amount,
+      showCounter: state.showCounter,
+    };
+  }
+
   if (action.type === 'decrement') {
-    return { counter: state.counter - 1 };
+    return {
+      counter: state.counter - 1,
+      showCounter: state.showCounter,
+    };
   }
+
+  if (action.type === 'toggle') {
+    return {
+      showCounter: !state.showCounter,
+      counter: state.counter,
+    };
+  }
+
   return state;
 };
 
-// // store 생성
+// store 생성
 const store = createStore(counterReducer);
-//
-// // subscriber 생성
-// const subscriber = redux.subscribe();
-//
-// // action...
-// const action = store.dispatch...
 
 export default store;
